cfa926ad30730849fcc2811b130f1b40428fbf47
Zlolka <jaroslavderkach@gmail.com>
2021-01-14 12:41:57 +0300

    Create first view

--- TestTask_FoxTree/urls.py
+++ TestTask_FoxTree/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r"^home", include("aggregator.urls")),
+    url(r"^Home", include("aggregator.urls")),
+    url(r"^", include("aggregator.urls")),
 ]
